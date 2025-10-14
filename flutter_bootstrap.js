ef9bee52b0023fc6b6dcf5a82dd68d95df134476
masscom dev <masscomdevicloud.com@masscoms-MacBook-Pro.local>
2025-10-14 09:29:23 +0700

    cm

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"ef0cd000916d64fa0c5d09cc809fa7ad244a57
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "480016344"
+    serviceWorkerVersion: "1087182317"
   }
 });
