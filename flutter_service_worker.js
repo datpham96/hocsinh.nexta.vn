2f9ff2d95943cc3c552b1e426289bb6ea0d60900
masscom dev <masscomdevicloud.com@masscoms-MacBook-Pro.local>
2025-10-14 19:30:26 +0700

    cm

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "22b349f0e14e3c037dd497433f446531",
+const RESOURCES = {"flutter_bootstrap.js": "138e3c5d95609ab17e4bcffb35efeac9",
 "version.json": "c51da51cb5bffb528cf3d2a536095dd5",
 "index.html": "d67d4e5e64f6b81258b2bb9443a326a2",
 "/": "d67d4e5e64f6b81258b2bb9443a326a2",
-"main.dart.js": "090cefa05256238e60abddde6a562a5d",
+"main.dart.js": "38b7c42998617c48637c2fab41c5aa52",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "0dff79908ffeff362e0e697091831efc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
