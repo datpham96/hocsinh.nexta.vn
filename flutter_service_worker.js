eed41cde10e3bb112892524f00444531360ae405
masscom dev <masscomdevicloud.com@masscoms-MacBook-Pro.local>
2025-10-21 14:30:36 +0700

    cm

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c47ce75c008930b8fd49b59fe85a3789",
+const RESOURCES = {"flutter_bootstrap.js": "df4012d00c5233f01a4cd41c967a6708",
 "version.json": "c51da51cb5bffb528cf3d2a536095dd5",
 "index.html": "d67d4e5e64f6b81258b2bb9443a326a2",
 "/": "d67d4e5e64f6b81258b2bb9443a326a2",
-"main.dart.js": "591a3d269b0625dda3c0a8fc4bc864e4",
+"main.dart.js": "dce816d32c5a3d8ada3afce07b7abc0c",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
 "favicon.png": "0dff79908ffeff362e0e697091831efc",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
@@ -17,7 +17,7 @@ const RESOURCES = {"flutter_bootstrap.js": "c47ce75c008930b8fd49b59fe85a3789",
 "manifest.json": "6aae1868ddcd7d582c39810d76d2afd6",
 "assets/shorebird.yaml": "e162e3ce90ce622dff8e9d210fef74c6",
 "assets/AssetManifest.json": "b5186aede41497f3b51f04a043b4423d",
-"assets/NOTICES": "15f26a0aa61ff6cfcb2108a9750c2e33",
+"assets/NOTICES": "fb71c442bc8c829e4fee72455fec0300",
 "assets/FontManifest.json": "5b6f0463f9c5af5c2300b97d905e1d6c",
 "assets/AssetManifest.bin.json": "0ede67298045937db882ad4586b93128",
 "assets/packages/flutter_math_fork/lib/katex_fonts/fonts/KaTeX_AMS-Regular.ttf": "657a5353a553777e270827bd1630e467",
